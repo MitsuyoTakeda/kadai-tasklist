cbbe1487228410e6d98714ed651253d2e8ebba6a
mitsuyo.takeda <bz88.0921.d03@gmail.com>
2020-01-27 21:04:01 +0900

    更新用ページ（フォームへの反映）まで作成

---
+++ src/contorollers/Edit.java
@@ -0,0 +1,53 @@
+package contorollers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import util.DBUtil;
+
+/**
+ * Servlet implementation class Edit
+ */
+@WebServlet("/edit")
+public class Edit extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Edit() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        // 該当のメッセージ1件を取得
+        Task tasks = em.find(Task.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        // メッセージをセッションスコープに格納
+        request.setAttribute("tasks", tasks);
+        request.setAttribute("_token", request.getSession().getId());
+
+        // メッセージIDをセッションスコープに登録
+        request.getSession().setAttribute("task_id", tasks.getId());
+
+        RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/tasks/edit.jsp");
+        rd.forward(request, response);
+    }
+
+}
