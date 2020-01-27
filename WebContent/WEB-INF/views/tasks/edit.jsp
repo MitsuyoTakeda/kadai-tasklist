9e2f0b6a3586ec0c664c3423b5650c95f114dd3b
mitsuyo.takeda <bz88.0921.d03@gmail.com>
2020-01-27 22:21:39 +0900

    更新処理を作成

--- WebContent/WEB-INF/views/tasks/edit.jsp
+++ WebContent/WEB-INF/views/tasks/edit.jsp
@@ -8,6 +8,18 @@
         </form>
 
         <a href="${pageContext.request.contextPath}/index">一覧に戻る</a>
+        <a href="#" onclick="confirmDestrory();">タスクを削除</a>
+        <form method="POST" action="${pageContext.request.contextPath }/destroy">
+            <imput type="hidden" name="_token" value="${tasks.id }" />
+        </form>
 
+        <script>
+            function confirmDestroy(){
+                if(confirm("本当に削除してよろしいですか？")){
+                    document.forms[1].submit();
+                }
+            }
+
+        </script>
     </c:param>
 </c:import>
---
+++ src/contorollers/Update.java
@@ -0,0 +1,64 @@
+package contorollers;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import util.DBUtil;
+
+/**
+ * Servlet implementation class Update
+ */
+@WebServlet("/update")
+public class Update extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Update() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = (String)request.getParameter("_token");
+
+        if(_token != null && _token.equals(request.getSession().getId())){
+            EntityManager em = DBUtil.createEntityManager();
+
+            // セッションスコープからタスクのIDを取得して
+            // 該当のIDのメッセージ1件のみを取得
+            Task task = em.find(Task.class, (Integer)(request.getSession().getAttribute("task_id")));
+
+            // フォームの内容を上書き
+            String content = request.getParameter("content");
+            task.setContent(content);
+
+            Timestamp update_at = new Timestamp(System.currentTimeMillis());
+            task.setUpdate_at(update_at);
+
+            // データベース更新
+            em.getTransaction().begin();
+            em.getTransaction().commit();
+            em.close();
+
+            // セッションスコープから不要になったIDを削除
+            request.getSession().removeAttribute("task_id");
+
+            // indexページにリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
+        }
+    }
+
+}
